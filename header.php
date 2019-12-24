9bd21f9851499126d6bf2268df98481e77afe9ff
beriya <57671960+beriya@users.noreply.github.com>
2019-12-24 05:57:51 -0800

    Add files via upload

--- header.php
+++ header.php
@@ -1,31 +1,58 @@
-<!DOCTYPE html>
+<?php
+/**
+ * The header for my theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package coffee_house
+ */
+
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset = "<?php bloginfo('charset'); ?>">
-        <meta name = "viewport" content = "width = device-width">
-        <title><?php bloginfo('name'); ?></title>
-        <?php wp_head(); ?>
-        
-    </head>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
 
 <body <?php body_class(); ?>>
-    <div class = "container">
-        <!-- site-header -->
-        <header class = "site-header">
-            <!-- hd search -->
-            <div class = "hd-search">
-                <?php get_search_form(); ?>
-            </div><!-- /hd-search --> 
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'coffee-house' ); ?></a>
 
-            <h1><a href = "<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-            <h5><?php bloginfo('description'); ?></h5>
-            <nav class ="site-nav">
-                <?php 
-                $args = array(
-                'theme_location' => 'primary'
-                );
-                ?>
-                <?php wp_nav_menu($args);?>
-            </nav>
-    </header><!--site-header -->
-             
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$coffee_house_description = get_bloginfo( 'description', 'display' );
+			if ( $coffee_house_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $coffee_house_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'coffee-house' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
