f4e9dadb935bb35c65514796846e5dfa02ac4f38
beriya <57671960+beriya@users.noreply.github.com>
2020-01-24 00:43:57 -0800

    Add files via upload

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,31 @@
-<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-    <div class="sidebar-module sidebar-module-inset">
-        <h4>About</h4>
-        <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-    </div>
-    <div class="sidebar-module">
-        <h4>Archives</h4>
-        <ol class="list-unstyled">
-            <li><a href="#">March 2014</a></li>
-            <li><a href="#">February 2014</a></li>
-            <li><a href="#">January 2014</a></li>
-            <li><a href="#">December 2013</a></li>
-            <li><a href="#">November 2013</a></li>
-            <li><a href="#">October 2013</a></li>
-            <li><a href="#">September 2013</a></li>
-            <li><a href="#">August 2013</a></li>
-            <li><a href="#">July 2013</a></li>
-            <li><a href="#">June 2013</a></li>
-            <li><a href="#">May 2013</a></li>
-            <li><a href="#">April 2013</a></li>
-        </ol>
-    </div>
-    <div class="sidebar-module">
-        <h4>Elsewhere</h4>
-        <ol class="list-unstyled">
-            <li><a href="#">GitHub</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Facebook</a></li>
-        </ol>
-    </div>
-</div><!-- /.blog-sidebar -->
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package coffee-house
+ */
+?>
+<div id="sidebar">    
+    <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+        <h3 class="widget-title"><?php esc_html_e( 'Category', 'skt-cafe' ); ?></h3>
+        <aside id="categories" class="widget">           
+            <ul>
+                <?php wp_list_categories('title_li=');  ?>
+            </ul>
+        </aside>
+       <h3 class="widget-title"><?php esc_html_e( 'Archives', 'skt-cafe' ); ?></h3>
+        <aside id="archives" class="widget">           
+            <ul>
+                <?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+            </ul>
+        </aside>
+         <h3 class="widget-title"><?php esc_html_e( 'Meta', 'skt-cafe' ); ?></h3>
+         <aside id="meta" class="widget">           
+            <ul>
+                <?php wp_register(); ?>
+                <li><?php wp_loginout(); ?></li>
+                <?php wp_meta(); ?>
+            </ul>
+        </aside>
+    <?php endif; // end sidebar widget area ?>	
+</div><!-- sidebar -->
